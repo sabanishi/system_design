c085db0d804d5e47438603f93add9a924b6ad3d7
sabanisi <taisuke140909@gmail.com>
2023-10-17 15:45:12 +0900

    課題2-3まで完了

--- httpserver/main.go
+++ httpserver/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net" // standard network package
+	"strings"
 )
 
 func main() {
@@ -43,6 +44,56 @@ func handleConnection(conn net.Conn) {
 	// close connection when this function ends
 	defer conn.Close()
 
-	// write response
-	conn.Write([]byte("Hello world."))
+	buf := make([]byte, 1024)
+	conn.Read(buf)
+
+	//HTTPリクエストを行毎に分割
+	splits := strings.Split(string(buf), "\n")
+	if len(splits) < 1 {
+		return
+	}
+
+	//アクセスURLが格納された1番目の要素を取り出す
+	resource := splits[0]
+	resources := strings.Split(resource, " ")
+
+	if len(resources) < 2 {
+		buf = createNotFoundRespose()
+	} else {
+		switch resources[1] {
+		case "/hello":
+			buf = createHttpResponse([]byte("Hello world."))
+		case "/bye":
+			buf = createHttpResponse([]byte("Good bye."))
+		case "/hello.jp":
+			buf = createHttpResponse([]byte("こんにちは"))
+		default:
+			buf = createNotFoundRespose()
+		}
+	}
+
+	conn.Write(buf)
+}
+
+/*引数のbyte列をBodyとするHTTPリクエストを生成する関数*/
+func createHttpResponse(body []byte) []byte {
+	buf := []byte(fmt.Sprintf(
+		"HTTP/1.1 200 OK\n"+
+			"Content-Type: text/plain; charset=utf-8\n"+
+			"Content-Length: %d\n"+
+			"\n"+
+			"%s\n",
+		len(body),
+		string(body)))
+	return buf
+}
+
+/*ページが見つからないことを示すHTTPリクエストを生成する関数*/
+func createNotFoundRespose() []byte {
+	buf := []byte(fmt.Sprintf(
+		"HTTP/1.1 404 Not Found\n"+
+			"%d\n"+
+			"\n",
+		0))
+	return buf
 }
