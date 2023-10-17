674e566981b800c68ec43538cf5621c04e69e3c6
sabanisi <taisuke140909@gmail.com>
2023-10-17 14:30:34 +0900

    GoLandセットアップ

--- main.go
+++ main.go
@@ -3,10 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	greet := hello("Masu")
-	fmt.Println(greet)
+	for i := 1; i <= 30; i++ {
+		fmt.Println("%d: %s", i, doFizzBuzz(i))
+	}
 }
 
-func hello(name string) string{
-	return fmt.Sprintf("Hello, %s!",name)
+func isTree(i int) bool {
+	return i%3 == 0
+}
+
+func isFive(i int) bool {
+	return i%5 == 0
+}
+
+func isFifteen(i int) bool {
+	return i%15 == 0
+}
+
+func doFizzBuzz(i int) string {
+	switch {
+	case isFifteen(i):
+		return "FizzBuzz"
+	case isTree(i):
+		return "Fizz"
+	case isFive(i):
+		return "Buzz"
+	default:
+		return fmt.Sprintf("%d", i)
+	}
 }
